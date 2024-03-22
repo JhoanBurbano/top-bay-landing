a8fda2cf5e462ada3a6db8dea865a17b87c60ca2
jsburbano <joans.burbano@gmail.com>
2024-03-22 09:36:55 -0500

    feat: add seo

--- src/constants/gallery.constants.ts
+++ src/constants/gallery.constants.ts
@@ -1,18 +1,18 @@
 export const gallery = [
     {
-        source: "https://cdn.durable.co/shutterstock/2blGRybuyvWBIO8UyO6RMFZ2RpmoAN7pfhKXcCCy39oBSL7x1clKCPJFBVAu13cx.jpeg",
+        source: "https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/freelance/images/cat-in-clean-bathroom.jpeg",
         alt: "The theme is luxury and wealth. A cat without a tail of the Mekong Bobtail breed in a retro bathroom in the interior of the Barocoo Versailles Palace. Jewel jewelery on the neck.",
     },
     {
-        source: "https://images.unsplash.com/photo-1615561916422-7014e1078997?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyNjI5NjF8MHwxfHNlYXJjaHwxM3x8Y2xlYW5pbmclMjBzZXJ2aWNlcyUyMGNvbnN1bHRpbmd8ZW58MHx8fHwxNzA2MjI3MzQ0fDA&ixlib=rb-4.0.3&q=80&w=1080",
+        source: "https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/freelance/images/man-in-airplane-talk-with-women.jpeg",
         alt: "man in blue dress shirt standing in airplane",
     },
     {
-        source: "https://cdn.durable.co/shutterstock/24OoRlEgG3DALGn3KyPUB1usH5j7ixXkaosEKzeHfB9GDuVmHEkNMk8C1jEf7App.jpeg",
+        source: "https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/freelance/images/clean-living-room.jpeg",
         alt: "Hotel lobby interior with reception desk, marble floor and crystal lamp..",
     },
     {
-        source: "https://images.unsplash.com/photo-1517837817850-607cd9ff1015?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wyNjI5NjF8MHwxfHNlYXJjaHwyM3x8Y2xlYW5pbmclMjBzZXJ2aWNlcyUyMGNvbnN1bHRpbmd8ZW58MHx8fHwxNzA2MjI3MzQ0fDA&ixlib=rb-4.0.3&q=80&w=1080",
+        source: "https://burbanostudio-assets.s3.us-east-2.amazonaws.com/projects/freelance/images/women-make-the-bed.jpeg",
         alt: "woman holding green textile",
     },
 ];
